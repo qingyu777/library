17a8b21c145c4868375afcd4bf58a548067b5b01
qingyu777 <1085015186@qq.com>
2019-06-01 17:53:18 +0800

    上次的补充

---
+++ library/borrowRecord.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+    class borrowRecord
+    {
+        private int borrow_record_id;
+        private String book_name;
+        private String book_borrow_time;
+
+        public int Borrow_record_id { get => borrow_record_id; set => borrow_record_id = value; }
+        public string Book_borrow_time { get => book_borrow_time; set => book_borrow_time = value; }
+        public string Book_name { get => book_name; set => book_name = value; }
+    }
+}
